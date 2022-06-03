005ace755ad11186d4958b1f65b0e5f104895edd
Диана Дудорова <sergejdudorov@MacBook-Pro-Sergej.local>
2022-06-03 18:48:35 +0300

    1st fix

--- scripts/popup.js
+++ scripts/popup.js
@@ -30,18 +30,3 @@ formElement.addEventListener('submit', function(event) {
     nameElement.textContent = nameFieldElement.value;
     aboutElement.textContent = aboutFieldElement.value;
 })
-
-for (let i = 0; i < likeActive.length; i++) {
-    likeActive[i].addEventListener("click", function() {
-        console.log(likeActive[i].classList)
-        if(likeActive[i].classList.contains('elements__like-button_active')){
-            likeActive[i].classList.remove('elements__like-button_active')
-        }
-        else{
-        likeActive[i].classList.add('elements__like-button_active')
-    }
-    });
-}
-
-
-
